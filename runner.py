ddbfaec0d644421fd1719b6a88f948f2dde7d8e3
tjw1184 <tjw1184@yahoo.com>
2022-02-13 12:55:43 -0800

    Update runner.py

--- runner.py
+++ runner.py
@@ -15,7 +15,7 @@ def main() -> None:
     
     # if no counter file copy the default
     if not path.exists("/configs/counter.txt"):
-        shutil.copyfile("/youtubedl/origconfigs/counter.txt","/configs/counter.txt") 
+        shutil.copyfile("/ytdlp/origconfigs/counter.txt","/configs/counter.txt") 
     
 #    # interval counter, default to 1 week 1 second (should be overrode by counter.txt)
 #    intervalcounter = float(604801.0)
@@ -31,43 +31,26 @@ def main() -> None:
 #     except:
 #        print("bad counter.txt file")
     
-#    print(f"Running youtubedl-auto every {intervalcounter}s", file=sys.stderr)
+#    print(f"Running ytdlp-auto every {intervalcounter}s", file=sys.stderr)
     while True:
 #        start_time = time.time()
         
         # if configs folder doesn't have config files then copy the defaults
-        if not path.exists("/configs/youtube-dl.conf"):
-            shutil.copyfile("/youtubedl/origconfigs/youtube-dl.conf","/configs/youtube-dl.conf")
-        if not path.exists("/configs/youtube-dl-archive.txt"):
-            shutil.copyfile("/youtubedl/origconfigs/youtube-dl-archive.txt","/configs/youtube-dl-archive.txt")
-        if not path.exists("/configs/youtube-dl-channels.txt"):
-            shutil.copyfile("/youtubedl/origconfigs/youtube-dl-channels.txt","/configs/youtube-dl-channels.txt")                     
-        
-        # Dirty hack to implement the 429 error workaround provided by colethedj, lock to 3-8-20 branch for now
-        # https://gitlab.com/colethedj/youtube-dl-429-patch
-        #prevdir = os.getcwd()
-        #os.chdir("/temp")
-        
-        # Updated youtube-dl broke the patch, so had to clone and hack the whole branch
-        #run(["/usr/bin/git","clone","https://github.com/ytdl-org/youtube-dl.git","-b","2020.03.08","--depth","1"])
-        #run(["/usr/bin/git","clone","https://gitlab.com/colethedj/youtube-dl-429-patch.git"])
-        #os.chdir("/temp/youtube-dl/youtube_dl")
-        #run(["/usr/bin/git","apply","../../youtube-dl-429-patch/youtube_dl_429.patch"])
-        #os.chdir("/temp/youtube-dl")
-        
-        #run(["/usr/bin/git","clone","https://github.com/tjw1184/youtube-dl-429.git","-b","2020.05.08.429fix","--depth","1"])
-        #os.chdir("/temp/youtube-dl-429")
-        #run(["/usr/local/bin/pip3","install","."])
-        #os.chdir(prevdir)
+        if not path.exists("/configs/ytdlp.conf"):
+            shutil.copyfile("/ytdlp/origconfigs/ytdlp.conf","/configs/ytdlp.conf")
+        if not path.exists("/configs/ytdlp-archive.txt"):
+            shutil.copyfile("/ytdlp/origconfigs/ytdlp-archive.txt","/configs/ytdlp-archive.txt")
+        if not path.exists("/configs/ytdlp-channels.txt"):
+            shutil.copyfile("/ytdlp/origconfigs/ytdlp-channels.txt","/configs/ytdlp-channels.txt")                     
         
         # re-enable pip install when hack no longer needed
-        run(["pip", "install", "--upgrade", "youtube-dl"])
+        run(["pip", "install", "--upgrade", "yt-dlp"])
         
         #open bash shell
         run(["/bin/bash"])
         
         # run youtubedl every interval seconds
-#        run(["/usr/local/bin/youtube-dl", "--config-location", "/configs/youtube-dl.conf"])
+#        run(["/usr/local/bin/yt-dlp", "--config-location", "/configs/ytdlp.conf"])
 #        run_time = time.time() - start_time
 #        if run_time < intervalcounter:
 #            sleep_time = intervalcounter - run_time
